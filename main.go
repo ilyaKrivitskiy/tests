ec7a4d8d1f6b559f52d66f711ea6399e239a1b38
ilyaKrivitskiy <krivitskiy-ilya@mail.ru>
2021-10-24 20:29:52 +0300

    sqrt-function-with-errors

--- main.go
+++ main.go
@@ -2,31 +2,45 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"math"
 )
 
-type IPAddr [4]byte
+type ErrNegativeSqrt float64
 
-// TODO: Add a "String() string" method to IPAddr.
-func (t IPAddr) String() string {
-	res := ""
-	dot := "."
-	for i := range t {
-		if i != len(t)-1 {
-			res += strconv.Itoa(int(t[i])) + dot
-			continue
-		}
-		res += strconv.Itoa(int(t[i]))
+func (e ErrNegativeSqrt) Error() string {
+	if e < 0 {
+		return fmt.Sprintf("cannot Sqrt negative number: %f", float64(e))
+	} else {
+		return ""
 	}
-	return res
+}
+
+func Sqrt(x float64) (float64, error) {
+	z := 1.0
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	} else if x == 0 {
+		return 0, nil
+	} else {
+		for math.Abs(z*z-x) > 0.00000001 {
+			z -= (z*z - x) / (2 * z)
+		}
+		return z, nil
+	}
 }
 
 func main() {
-	hosts := map[string]IPAddr{
-		"loopback":  {127, 0, 0, 1},
-		"googleDNS": {8, 8, 8, 8},
+	two, negTwo := 2, -2
+	res1, err := Sqrt(float64(two))
+	if err != nil {
+		panic(err)
 	}
-	for name, ip := range hosts {
-		fmt.Printf("%v: %v\n", name, ip)
+	fmt.Printf("Sqrt of %f = %f \n", float64(two), res1)
+
+	res2, err2 := Sqrt(float64(negTwo))
+	if err2 != nil {
+		panic(err2)
 	}
+	fmt.Printf("Sqrt of %f = %f", float64(negTwo), res2)
+
 }
